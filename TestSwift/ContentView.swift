02d2b590cb8a13d9d6a98edda24e50973e3c6e39
Yasuo Ashina <ashipp3000@gmail.com>
2020-12-08 21:47:17 +0900

    init

---
+++ TestSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSwift
+//
+//  Created by aship on 2020/11/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
